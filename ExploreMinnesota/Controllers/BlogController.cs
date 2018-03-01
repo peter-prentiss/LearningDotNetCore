ae82db3fd759f17e368642c819a344d13e7a3243
Peter <peter.prentiss@gmail.com>
2018-03-01 14:56:59 -0600

    Render dynamic url.

--- ExploreMinnesota/Controllers/BlogController.cs
+++ ExploreMinnesota/Controllers/BlogController.cs
@@ -17,7 +17,7 @@ namespace ExploreMinnesota.Controllers
             return new ContentResult { Content = "Blog posts" };
         }
 
-        [Route("blog/{year:min(2000}/{month:range(1,12}/{key}")]
+        [Route("blog/{year:min(2000)}/{month:range(1,12)}/{key}")]
         public IActionResult Post(int year, int month, string key)
         {
             return new ContentResult { Content = string.Format("Year: {0}; Month: {1}; Key: {2}", year, month, key) };
