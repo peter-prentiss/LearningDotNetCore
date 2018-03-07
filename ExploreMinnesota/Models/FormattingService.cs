7355358f24631688c8a414ed1610a98d5b958389
Peter <peter.prentiss@gmail.com>
2018-03-06 21:44:12 -0600

    Turn date into reusable service

---
+++ ExploreMinnesota/Models/FormattingService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ExploreMinnesota.Models
+{
+    public class FormattingService
+    {
+        public string AsReadableDate(DateTime date)
+        {
+            return date.ToString("d");
+        }
+    }
+}
--- ExploreMinnesota/Startup.cs
+++ ExploreMinnesota/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using ExploreMinnesota.Models;
 
 namespace ExploreMinnesota
 {
@@ -28,6 +29,8 @@ namespace ExploreMinnesota
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<FormattingService>();
+
             services.AddTransient<FeatureToggles>(x => new FeatureToggles
             {
                 EnableDeveloperExceptions =
