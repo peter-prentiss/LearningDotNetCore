8bbb1611f72c58c255c6ae1e097a41f47abe35af
Peter <peter.prentiss@gmail.com>
2018-03-06 22:15:20 -0600

    Convert monthly specials into reusable view component.

--- ExploreMinnesota/Startup.cs
+++ ExploreMinnesota/Startup.cs
@@ -29,6 +29,8 @@ namespace ExploreMinnesota
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<SpecialsDataContext>();
+
             services.AddTransient<FormattingService>();
 
             services.AddTransient<FeatureToggles>(x => new FeatureToggles
---
+++ ExploreMinnesota/ViewComponents/MonthlySpecialsViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using ExploreMinnesota.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+namespace ExploreMinnesota.ViewComponents
+{
+    [ViewComponent]
+    public class MonthlySpecialsViewComponent : ViewComponent
+    {
+        private readonly SpecialsDataContext _specials;
+
+        public MonthlySpecialsViewComponent(SpecialsDataContext specials) => _specials = _specials;
+
+        public IViewComponentResult Invoke()
+        {
+            var specials = _specials.GetMonthlySpecials();
+            return View(specials);
+        }
+    }
+}
