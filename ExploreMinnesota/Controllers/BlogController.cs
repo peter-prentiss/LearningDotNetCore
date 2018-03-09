c97e1899e2f9e38235f6a7c57aa60e29eca7032b
Peter <peter.prentiss@gmail.com>
2018-03-08 21:42:27 -0600

    Post form data to controller

--- ExploreMinnesota/Controllers/BlogController.cs
+++ ExploreMinnesota/Controllers/BlogController.cs
@@ -50,10 +50,18 @@ namespace ExploreMinnesota.Controllers
             return View(post);
         }
 
-        [Route("create")]
+        [HttpGet, Route("create")]
         public IActionResult Create()
         {
             return View();
         }
+
+        [HttpPost, Route("create")]
+        public IActionResult Create(Post post)
+        {
+            post.Author = User.Identity.Name;
+            post.Posted = DateTime.Now;
+            return View();
+        } 
     }
 }
