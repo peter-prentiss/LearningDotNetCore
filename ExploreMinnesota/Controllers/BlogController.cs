c85a5ee71c44da82d059e3b6fdd5a5af22c4c4b4
Peter <peter.prentiss@gmail.com>
2018-03-09 22:20:40 -0600

    New posts write to database.

--- ExploreMinnesota/Controllers/BlogController.cs
+++ ExploreMinnesota/Controllers/BlogController.cs
@@ -12,6 +12,13 @@ namespace ExploreMinnesota.Controllers
     [Route("blog")]
     public class BlogController : Controller
     {
+        private readonly BlogDataContext _db;
+
+        public BlogController(BlogDataContext db)
+        {
+            _db = db;
+        }
+
         [Route("")]
         public IActionResult Index()
         {
@@ -64,6 +71,9 @@ namespace ExploreMinnesota.Controllers
 
             post.Author = User.Identity.Name;
             post.Posted = DateTime.Now;
+
+            _db.Posts.Add(post);
+            _db.SaveChanges(); 
 
             return View();
         } 
