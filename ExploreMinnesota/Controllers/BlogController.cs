4804ceed72d87ee4c744230e78fdd64cca01df5e
Peter <peter.prentiss@gmail.com>
2018-03-09 22:28:38 -0600

    Get and display posts from database.

--- ExploreMinnesota/Controllers/BlogController.cs
+++ ExploreMinnesota/Controllers/BlogController.cs
@@ -22,23 +22,7 @@ namespace ExploreMinnesota.Controllers
         [Route("")]
         public IActionResult Index()
         {
-            var posts = new[]
-            {
-                new Post
-                {
-                    Title = "My blog post",
-                    Posted = DateTime.Now,
-                    Author = "Peter Prentiss",
-                    Body = "This is a great blog post, don't you think?",
-                },
-                new Post
-                {
-                    Title = "My second blog post",
-                    Posted = DateTime.Now,
-                    Author = "Peter Prentiss",
-                    Body = "This is ANOTHER great blog post, don't you think?",
-                },
-            };
+            var posts = _db.Posts.OrderByDescending(x => x.Posted).Take(5).ToArray();
 
             return View(posts);
         }
@@ -46,13 +30,7 @@ namespace ExploreMinnesota.Controllers
         [Route("{year:min(2000)}/{month:range(1,12)}/{key}")]
         public IActionResult Post(int year, int month, string key)
         {
-            var post = new Post
-            {
-                Title = "My blog post",
-                Posted = DateTime.Now,
-                Author = "Peter Prentiss",
-                Body = "This is a great blog post, don't you think?",
-            };
+            var post = _db.Posts.FirstOrDefault(x => x.Key == key);
 
             return View(post);
         }
@@ -73,9 +51,14 @@ namespace ExploreMinnesota.Controllers
             post.Posted = DateTime.Now;
 
             _db.Posts.Add(post);
-            _db.SaveChanges(); 
+            _db.SaveChanges();
 
-            return View();
+            return RedirectToAction("Post", "Blog", new
+            {
+                year = post.Posted.Year,
+                month = post.Posted.Month,
+                key = post.Key
+            });
         } 
     }
 }
